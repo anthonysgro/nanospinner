6846a97398818af7ef7245701ee8685eae86675e
Anthony Sgro <anthony.m.sgro@gmail.com>
2026-02-22 16:13:57 -0500

    Add demo tape

--- examples/demo.rs
+++ examples/demo.rs
@@ -5,18 +5,22 @@ use std::time::Duration;
 fn main() {
     // 1. Success Demo
     let handle = Spinner::new("Downloading the internet...").start();
-    thread::sleep(Duration::from_secs(2));
+    thread::sleep(Duration::from_secs(3));
     handle.success();
+
+    thread::sleep(Duration::from_secs(1));
 
     // 2. Update Demo
     let handle = Spinner::new("Initializing...").start();
-    thread::sleep(Duration::from_secs(1));
+    thread::sleep(Duration::from_secs(2));
     handle.update("Still initializing, but faster...");
-    thread::sleep(Duration::from_secs(1));
+    thread::sleep(Duration::from_secs(2));
     handle.success_with("Ready!");
+
+    thread::sleep(Duration::from_secs(1));
 
     // 3. Fail Demo
     let handle = Spinner::new("Attempting to bypass physics...").start();
-    thread::sleep(Duration::from_secs(2));
+    thread::sleep(Duration::from_secs(3));
     handle.fail_with("Laws of thermodynamics won.");
 }
